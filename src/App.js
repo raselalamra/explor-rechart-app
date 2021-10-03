ca49a2077989de26607ee5ddd988f2dbbefbed94
raselalamjubayed <raselalam838@gmil.com>
2021-10-03 20:52:21 +0800

    rechart added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyLineChart from './components/MyLineChart/MyLineChart';
+import TreeMapChart from './components/TreeMapChart/TreeMapChart';
+import ContainerChart from './components/ContainerChart/ContainerChart';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyLineChart></MyLineChart>
+      <TreeMapChart></TreeMapChart>
+      <ContainerChart></ContainerChart>
     </div>
   );
 }
